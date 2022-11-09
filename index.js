61f51320b17842cdde670c289a835a5762840c3d
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-10 05:23:23 +0600

    create and collection

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.nl9uncn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl9uncn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+async function run(){
+    try{
+        const serviceCollection = client.db("upturn").collection("services");
+
+        app.get('/limitServices', async (req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
