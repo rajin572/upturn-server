ac304f5db1f72d4f671d6fd3552524f7787350cf
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-10 13:58:18 +0600

    resolve the problem

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl9uncn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 async function run(){
     try{
         const serviceCollection = client.db("upturn").collection("services");
