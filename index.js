a386e96154fb18cc6a952a42c19d8108ce153259
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-10 07:40:50 +0600

    create server for load one data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express()
@@ -27,6 +27,18 @@ async function run(){
             const cursor = serviceCollection.find(query)
             const result = await cursor.limit(3).toArray()
             res.send(result)
+        })
+        app.get('/services', async (req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
         })
     }
     finally{
