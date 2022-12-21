015c113163c255bc8f1d54340c0b67a4f19250ff
Din Islam Rajin <dirrazin572@gmail.com>
2022-12-21 23:35:58 +0600

    change

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const {
+    MongoClient,
+    ServerApiVersion,
+    ObjectId
+} = require('mongodb');
 require('dotenv').config();
 
 const app = express()
@@ -12,15 +16,19 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl9uncn.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 
-async function run(){
-    try{
+async function run() {
+    try {
         const serviceCollection = client.db("upturn").collection("services");
         const reviewCollection = client.db("upturn").collection("reviews");
 
         // to get only 3 service data from database 
-        app.get('/limitServices', async (req, res) =>{
+        app.get('/limitServices', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const result = await cursor.limit(3).toArray()
@@ -28,7 +36,7 @@ async function run(){
         })
 
         // to get all service data from database
-        app.get('/services', async (req, res) =>{
+        app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const result = await cursor.toArray()
@@ -36,7 +44,7 @@ async function run(){
         })
 
         //to post new service on the website and database
-        app.post('/services', async (req, res) =>{
+        app.post('/services', async (req, res) => {
             const order = req.body;
             const result = await serviceCollection.insertOne(order)
             res.send(result)
@@ -44,15 +52,17 @@ async function run(){
         })
 
         // to get the chlicked data by its id 
-        app.get('/services/:id', async(req, res)=>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: ObjectId(id)}
+            const query = {
+                _id: ObjectId(id)
+            }
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
 
         //to post reviews on the database from the website
-        app.post('/reviews', async (req, res) =>{
+        app.post('/reviews', async (req, res) => {
             const order = req.body;
             const result = await reviewCollection.insertOne(order)
             res.send(result)
@@ -60,16 +70,16 @@ async function run(){
         })
 
         //to get reviews data by query from database
-        app.get('/reviews', async (req, res) =>{
-            let query ={}
-            if(req.query.email){
+        app.get('/reviews', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
                 query = {
-                    email: req.query.email 
+                    email: req.query.email
                 }
             }
-            if(req.query.service){
+            if (req.query.service) {
                 query = {
-                    service: req.query.service 
+                    service: req.query.service
                 }
             }
             const cursor = reviewCollection.find(query)
@@ -78,14 +88,15 @@ async function run(){
         })
 
         //to delete a particular review from website and databse
-        app.delete('/reviews/:id', async (req, res) =>{
+        app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = {
+                _id: ObjectId(id)
+            }
             const result = await reviewCollection.deleteOne(query)
             res.send(result)
         })
-    }
-    finally{
+    } finally {
 
     }
 }
@@ -97,10 +108,10 @@ run().catch(error => console.error(error))
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('upturn server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
